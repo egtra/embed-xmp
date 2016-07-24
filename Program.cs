c1101841a41d6910a71008ec761ca52170c2cfca
Yusuke Ichinohe <yusuke.ichinohe@gmail.com>
2016-07-25 00:27:27 +0900

    First implementation

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+using iTextSharp.text.pdf;
 
 namespace EmbedXMP
 {
@@ -10,6 +9,38 @@ namespace EmbedXMP
     {
         static void Main(string[] args)
         {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Usage: EmbedXMP input-PDF input-XMP output-PDF");
+                return;
+            }
+            EmbedXmpToPdf(args[0], args[1], args[2]);
+        }
+
+        static void EmbedXmpToPdf(string inputPdfFile, string inputXmpFile, string outputPdfFile)
+        {
+            var pdfReader = new PdfReader(inputPdfFile);
+            using (var output = File.Create(outputPdfFile))
+            using (var pdfStamper = new PdfStamper(pdfReader, output))
+            {
+                pdfStamper.SetFullCompression();
+                pdfStamper.XmpMetadata = MakeXmpPacket(inputXmpFile);
+            }
+        }
+
+        static byte[] MakeXmpPacket(string inputXmpFile)
+        {
+            var meta = XNamespace.Get("adobe:ns:meta/");
+            var input = XDocument.Load(inputXmpFile);
+            var xmpmeta = input.Element(meta + "xmpmeta");
+            var output = new XDocument(
+                new XProcessingInstruction("xpacket", "begin='\xFEFF' id='W5M0MpCehiHzreSzNTczkc9d'"),
+                xmpmeta,
+                new XProcessingInstruction("xpacket", "end='r'")
+            );
+            var s = new MemoryStream();
+            output.Save(s, SaveOptions.DisableFormatting);
+            return s.ToArray();
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EmbedXMP")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © Egtra 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
